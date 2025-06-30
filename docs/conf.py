725a8719cd12ff841c55d3045eee719d37fc300d
Dinghow Yang <dinghaoy@nvidia.com>
2025-06-30 15:52:30 +0800

    Add import path to sphinx conf (#46)

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,11 @@
+import os
+import sys
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
