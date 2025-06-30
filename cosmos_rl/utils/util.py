Fix logprob (#41)

cosmos_rl/utils/util.py
@@ -999,9 +999,7 @@ def compute_logprobs(
     ), f"Logits shape {full_logits.shape} does not match input_ids shape {shifted_input_ids.shape}"
     bsz, _, _ = full_logits.shape
     # select the effective logits
-    effective_logits = torch.gather(
-        full_logits,
-    )  # [n_logprob_tokens, vocab_size]
+    effective_logits = full_logits[logprob_masks]  # [n_logprob_tokens, vocab_size]
     effective_input_ids = shifted_input_ids[logprob_masks]  # [n_logprob_tokens,]
     masked_seqlens = logprob_masks.sum(dim=-1)  # [bsz,]
     cu_seqlens = torch.zeros(