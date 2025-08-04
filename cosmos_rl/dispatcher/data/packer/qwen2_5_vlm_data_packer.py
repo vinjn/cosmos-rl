fix: GRPO position id computation (#191)

cosmos_rl/dispatcher/data/packer/qwen2_5_vlm_data_packer.py
@@ -597,8 +597,25 @@ class Qwen2_5_VLM_DataPacker(DataPacker):
         completion_ids = []
         if rollout_output:
             completion_ids = self.tokenizer(rollout_output).input_ids
-
-        return_dict["input_ids"] = input_ids + completion_ids
+            # recompute position_ids
+            # position_ids: (3, 1, seq_len)
+            position_ids, _ = self._get_rope_index(
+                input_ids=torch.tensor(input_ids + completion_ids).unsqueeze(0).clone(),
+                image_grid_thw=torch.tensor(x.get("image_grid_thw"))
+                if "image_grid_thw" in x
+                else None,
+                video_grid_thw=torch.tensor(x.get("video_grid_thw"))
+                if "video_grid_thw" in x
+                else None,
+                second_per_grid_ts=torch.tensor(x.get("second_per_grid_ts"))
+                if "second_per_grid_ts" in x
+                else None,
+                attention_mask=None,
+            )
+            return_dict["position_ids"] = position_ids.clone()
+            return_dict["input_ids"] = input_ids + completion_ids
+        else:
+            return_dict["input_ids"] = input_ids
 
         return_dict["logprob_masks"] = (
             [0] * (len(input_ids) - 1 + n_ignore_prefix_tokens)

cosmos_rl/policy/trainer/grpo_trainer.py
@@ -1105,7 +1105,10 @@ class GRPOTrainer(Trainer):
             "logprob_masks" in minibatch
         ), "logprob_masks is required for computing logprobs"
         return logprobs_computing(
-            minibatch["input_ids"], minibatch["logprob_masks"], full_logits
+            minibatch["input_ids"],
+            minibatch["logprob_masks"],
+            full_logits,
+            self.tokenizer,
         )
 
     @torch.no_grad()
@@ -1471,13 +1474,12 @@ class GRPOTrainer(Trainer):
                                 local_mini_step
                                 % int(os.environ.get("COSMOS_GRPO_STEP_INTERVAL", 10))
                                 == 0
-                            ):
+                            ) and local_mini_step > 1:
+                                all_reduced = True
                                 self.execute_all_reduce()
-                        if not is_computing_ref and (
-                            local_mini_step
-                            % int(os.environ.get("COSMOS_GRPO_STEP_INTERVAL", 10))
-                            != 0
-                        ):
+                            else:
+                                all_reduced = False
+                        if not is_computing_ref and not all_reduced:
                             self.execute_all_reduce()
         self.old_per_token_logps = []
         self.ref_per_token_logps = []